7f8367b5e9eba611f5f3db3df5933ca29e22f600
Neculai Alexandru <alleexx250595@gmail.com>
2022-03-28 20:39:58 +0300

    GUI Converter using tkinter completed

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def convert_to_km():
+    miles_converted = round(float(miles_input.get()) * 1.609)
+    kilometer_result_label.config(text=f"{miles_converted}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window = Tk()
+window.title("Mile to Km Converter")
+# window.minsize(width=300, height=100)
+window.config(padx=20, pady=20)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+GLOBAL_FONT = ("Arial", 12, "normal")
+
+# textbox for user to input the amount of miles to be converted
+miles_input = Entry(width=5, font=GLOBAL_FONT, justify="center")
+miles_input.grid(row=0, column=1)
+miles_input.insert(END, "0")
+
+miles_label = Label(text="Miles", font=GLOBAL_FONT)
+miles_label.grid(row=0, column=2)
+
+is_equal_label = Label(text="is equal to", font=GLOBAL_FONT)
+is_equal_label.grid(row=1, column=0)
+
+kilometer_result_label = Label(text="0", font=GLOBAL_FONT)
+kilometer_result_label.grid(row=1, column=1)
+
+kilometer_label = Label(text="Km", font=GLOBAL_FONT)
+kilometer_label.grid(row=1, column=2)
+
+calculate_button = Button(text="Calculate", command=convert_to_km)
+calculate_button.grid(row=2, column=1)
+
+window.mainloop()
